4447ca82d9c293ef7e30c99cf01a188d4fdb7811
Shashank Bansal <shashank.bansal_cs19@gla.ac.in>
2020-08-09 21:36:03 +0530

    initial commit

---
+++ src/MethodDemo.java
@@ -0,0 +1,15 @@
+public class MethodDemo {
+    public static void main(String[] args) {
+        System.out.println("method demo");
+        MethodDemo object = new MethodDemo();
+        object.Display();
+        System.out.println("existing...");
+    }
+
+    public void Display() {
+        System.out.println("Hello World");
+
+
+    }
+}
+
